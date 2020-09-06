0b2f38917bfe5e860d5aab8c13098cb27c958d07
Shane Koehler <shane.koehler@gmail.com>
2020-09-06 04:38:43 -0500

    added simple mailto contact form

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { Card, CardTitle, CardText } from "../components/Card";
 
 function Contact() {
   return (
-    <div>contact</div>
+    <Card>
+      <CardTitle title="Contact" extClass="text-left" />
+      <hr />
+      <CardText>
+        <form action="mailto:shmoesolid@gmail.com">
+          <div class="form-group">
+              <input type="text" class="form-control" id="name" placeholder="Enter your name" name="name" />
+              <br />
+              <textarea class="form-control" id="message" placeholder="Enter your message" rows="5"></textarea>
+              <br />
+              <button type="submit" class="btn btn-dark">Submit</button>
+          </div>
+        </form>
+      </CardText>
+    </Card>
   );
 }
 
